c470203480d5b71b377a6b3757a3df0fe265892e
Sharath <172364102+CKSharath@users.noreply.github.com>
2025-11-22 10:23:40 +0530

    Create 6.PriceToEarningsAndEarningsPerShare.py

---
+++ 6.PriceToEarningsAndEarningsPerShare.py
@@ -0,0 +1,20 @@
+# Earnings Per Share (EPS) =Earnings/SharesOut if the company does't give any dividends
+# if it does          EPS=((Net income)-(preferred dividends))/SharesOut
+
+#Eg:        company A           company B
+#Earnings     $100M               $100M
+#SharesOut    10M                 50M
+#ESP          $10/share           $2/share 
+
+#company A is a better deal since EPS is higher than B
+
+# High EPS => Good
+# Low EPS => Bad
+#EPS(A) = +5.6  eps is +ve then company A is good and making money
+#EPS(B) = -3.8  eps is +ve then company B is bad and losing money
+
+#Price to Earnings (P/E)
+
+# P     MarketCap   Shares*Price    Price
+# -- =  --------- = ------------ =  ------
+# E     Earnings    Shares*EPS      EPS
