887c4aba0e00119d6a6a1be566db5e59862ec9ac
Sharath <172364102+CKSharath@users.noreply.github.com>
2025-11-23 10:08:36 +0530

    Update 7.PEGRatiovsPriceToEarningsRatio.py

--- 7.PEGRatiovsPriceToEarningsRatio.py
+++ 7.PEGRatiovsPriceToEarningsRatio.py
@@ -1,1 +1,18 @@
+# PEG=(P/E)/EGR  P/E = Price/EPS  #EGR=>Earnings growth rate
+# if PEG^ => stock overvalued
+# if PEG low => stock undervalued
 
+#Eg:        company A       company B
+#Price          $50            $50
+#EPS            2.50           5.0
+#P/E            20             10   
+#EGR            20%            5%
+#------------------------------------
+#PEG            1              2
+
+#according to P/E B is undervalued 
+#but according PEG A is undervalued
+
+#PEG is better than P/E because it cantain EGR(how fast a company can grow its earning) param hence give a better overview
+
+# !! It is not gurantee but just a prediction !!
